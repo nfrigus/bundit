8d66d8dce17205b3d77b873bc07b2883ad6636e9
Semen Shestakov <shestakovsemen@gmail.com>
2017-02-24 14:01:55 +0200

    Add less bundler

--- index.js
+++ index.js
@@ -1,46 +1,15 @@
 const
-  add = require('gulp-add'),
-  babel = require('babelify'),
-  browserify = require('browserify'),
-  buffer = require('vinyl-buffer'),
-  concat = require('gulp-concat'),
-  gulp = require('gulp'),
-  path = require('path'),
-  source = require('vinyl-source-stream'),
-  sourcemaps = require('gulp-sourcemaps'),
-  uglify = require('gulp-uglify');
+  packJs = require('./lib/pack-js'),
+  packLess = require('./lib/pack-less');
 
-module.exports = function (src, dest, opt = {}) {
-  dest = path.parse(dest);
+module.exports = (...args) => {
+  console.info('Calling bundit directly is deprecated and will be removed in future versions.');
+  console.info('Use bundit.packJs(...) instead.');
 
-  return function () {
-    let stream = browserify(src, {
-      debug: true
-    })
-      .transform(babel, {
-        presets: ["es2015"],
-        sourceMaps: true
-      })
-      .bundle()
-      .on('error', function (err) {
-        console.error(err);
-        this.emit('end');
-      })
-      .pipe(source(dest.base))
-      .pipe(buffer());
-
-    if (opt.license) {
-      stream = stream
-        .pipe(add('LICENSE', opt.license, true))
-    }
-
-    stream
-      .pipe(sourcemaps.init({
-        loadMaps: true
-      }))
-      .pipe(concat(dest.base))
-      .pipe(uglify({preserveComments: 'license'}))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest(dest.dir));
-  }
+  return packJs(...args);
 };
+
+Object.assign(module.exports, {
+  packJs,
+  packLess,
+});
